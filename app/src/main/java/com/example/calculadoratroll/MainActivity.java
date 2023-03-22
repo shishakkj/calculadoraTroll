1ccab5c7cbf513cdf38b8df61b3d665c55b1f4d5
shishakkj <franciscagoetz09@gmail.com>
2023-03-22 10:47:02 -0300

    Funções adicionadas

--- app/src/main/java/com/example/calculadoratroll/MainActivity.java
+++ app/src/main/java/com/example/calculadoratroll/MainActivity.java
@@ -15,10 +15,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        getActionBar().hide();
+        getSupportActionBar().hide();
         valor1 = findViewById(R.id.valor1);
         valor2 = findViewById(R.id.valor2);
         resultado = findViewById(R.id.resultado);
+    }
+    public void clear (View v){
+        valor1.getText().clear();
+        valor2.getText().clear();
+        resultado.setText("");
     }
     public void somar (View v){
         int intValor1 = Integer.parseInt(valor1.getText().toString());
@@ -26,6 +31,45 @@ public class MainActivity extends AppCompatActivity {
         int soma = intValor1 + intValor2;
         String resultadoSoma = String.valueOf(soma);
         resultado.setText(resultadoSoma);
+    }
+    public void subtrair (View v){
+        int intValor1 = Integer.parseInt(valor1.getText().toString());
+        int intValor2 = Integer.parseInt(valor2.getText().toString());
+        int subtracao = intValor1 - intValor2;
+        String resultadoSub = String.valueOf(subtracao);
+        resultado.setText(resultadoSub);
+    }
+
+    public void multiplicar (View v){
+        int intValor1 = Integer.parseInt(valor1.getText().toString());
+        int intValor2 = Integer.parseInt(valor2.getText().toString());
+        int multiplicacao = intValor1 * intValor2;
+        String resultadoMulti = String.valueOf(multiplicacao);
+        resultado.setText(resultadoMulti);
 
     }
+
+    public void dividir (View v){
+        int intValor1 = Integer.parseInt(valor1.getText().toString());
+        int intValor2 = Integer.parseInt(valor2.getText().toString());
+        int divisao = intValor1/intValor2;
+        String resultadoDivisao = String.valueOf(divisao);
+        resultado.setText(resultadoDivisao);
+    }
+
+    public void exponenciar (View v){
+        int intValor1 = Integer.parseInt(valor1.getText().toString());
+        int intValor2 = Integer.parseInt(valor2.getText().toString());
+        double potencia = Math.pow(intValor1,intValor2);
+        String resultadoPotencia = String.valueOf(potencia);
+        resultado.setText(resultadoPotencia);
+    }
+    public void raiz (View v){
+        int intValor1 = Integer.parseInt(valor1.getText().toString());
+        double raiz = Math.sqrt(intValor1);
+        String resultadoRaiz = String.valueOf(raiz);
+        resultado.setText(resultadoRaiz);
+
+    }
+
 }
