db68a771b83ba1d2654e8944c4c626dbe8294743
FRANCISCA CLARA GOETZ TEIXEIRA <francisca_c_teixeira@estudante.sesisenai.org.br>
2023-03-22 08:41:36 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/calculadoratroll/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.calculadoratroll;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText valor;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getActionBar().hide();
+    }
+    public void somar (View v){
+
+    }
+}
